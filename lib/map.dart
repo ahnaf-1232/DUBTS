59ced6bf615f0793354e9ba01d6ac54853a3028f
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2023-06-26 22:15:27 +0600

    Updated permission
    Added Permission feature to track location.

--- lib/map.dart
+++ lib/map.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:geolocator/geolocator.dart';
 
 class MapTracker extends StatefulWidget {
@@ -19,16 +18,35 @@ class _MapTrackerState extends State<MapTracker> {
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _startLocationTracking();
   }
 
-  Future<void> _getCurrentLocation() async {
+  Future<void> _startLocationTracking() async {
+    final LocationPermission permission = await _getLocationPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      // Handle the case when the user denies location permission
+      return;
+    }
+
     final position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
+
     setState(() {
       _currentLocation = LatLng(position.latitude, position.longitude);
     });
+  }
+
+  Future<LocationPermission> _getLocationPermission() async {
+    if (await Permission.location.isGranted) {
+      return LocationPermission.always;
+    } else {
+      final PermissionStatus status = await Permission.location.request();
+      return status.isGranted
+          ? LocationPermission.always
+          : LocationPermission.denied;
+    }
   }
 
   @override
@@ -37,26 +55,26 @@ class _MapTrackerState extends State<MapTracker> {
       home: FlutterMap(
         options: MapOptions(
           center:
-              _currentLocation ?? LatLng(23.6850, 90.3563), // Centered on Dhaka
+          _currentLocation ?? LatLng(23.6850, 90.3563), // Centered on Dhaka
           zoom: 8.0, // Initial zoom level
         ),
         children: [
           TileLayer(
             urlTemplate:
-                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap provider
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap provider
             subdomains: ['a', 'b', 'c'],
           ),
           MarkerLayer(
             markers: _currentLocation != null
                 ? [
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: _currentLocation!,
-                      builder: (ctx) =>
-                          Icon(Icons.location_on, color: Colors.red),
-                    ),
-                  ]
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: _currentLocation!,
+                builder: (ctx) =>
+                    Icon(Icons.location_on, color: Colors.red),
+              ),
+            ]
                 : [],
           ),
         ],
