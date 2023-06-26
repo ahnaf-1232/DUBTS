543bcaf17cb02ae6220035e41c63a811c84680dc
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2023-06-26 22:08:14 +0600

    Update map.dart

--- lib/map.dart
+++ lib/map.dart
@@ -14,96 +14,52 @@ class MapTracker extends StatefulWidget {
 }
 
 class _MapTrackerState extends State<MapTracker> {
-  late MapController _mapController;
-  Stream<LatLng>? _locationStream;
   LatLng? _currentLocation;
 
   @override
   void initState() {
     super.initState();
-    _mapController = MapController();
-    _startLocationTracking();
+    _getCurrentLocation();
   }
 
-  Future<void> _startLocationTracking() async {
-    final LocationPermission permission = await _getLocationPermission();
-    if (permission == LocationPermission.denied ||
-        permission == LocationPermission.deniedForever) {
-      // Handle the case when the user denies location permission
-      return;
-    }
-
+  Future<void> _getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
     setState(() {
-      _locationStream = Geolocator.getPositionStream().map((position) {
-        return LatLng(position.latitude, position.longitude);
-      });
+      _currentLocation = LatLng(position.latitude, position.longitude);
     });
-  }
-
-  Future<LocationPermission> _getLocationPermission() async {
-    if (await Permission.location.isGranted) {
-      return LocationPermission.always;
-    } else {
-      final PermissionStatus status = await Permission.location.request();
-      return status.isGranted ? LocationPermission.always : LocationPermission.denied;
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: StreamBuilder<LatLng>(
-          stream: _locationStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              _currentLocation = snapshot.data;
-              _mapController.move(_currentLocation!, 13.5);
-            }
-
-            return FlutterMap(
-              mapController: _mapController,
-              options: MapOptions(
-                center: _currentLocation ?? LatLng(51.509364, -0.128928),
-                zoom: 9.2,
-              ),
-              nonRotatedChildren: [
-                RichAttributionWidget(
-                  attributions: [
-                    TextSourceAttribution(
-                      'OpenStreetMap contributors',
-                      onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'com.example.app',
-                ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      width: 40.0,
-                      height: 40.0,
-                      point: _currentLocation ?? LatLng(51.509364, -0.128928),
-                      builder: (ctx) => Container(
-                        child: Icon(
-                          Icons.location_pin,
-                          color: Colors.red,
-                          size: 40.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            );
-          },
+      home: FlutterMap(
+        options: MapOptions(
+          center:
+              _currentLocation ?? LatLng(23.6850, 90.3563), // Centered on Dhaka
+          zoom: 8.0, // Initial zoom level
         ),
+        children: [
+          TileLayer(
+            urlTemplate:
+                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap provider
+            subdomains: ['a', 'b', 'c'],
+          ),
+          MarkerLayer(
+            markers: _currentLocation != null
+                ? [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: _currentLocation!,
+                      builder: (ctx) =>
+                          Icon(Icons.location_on, color: Colors.red),
+                    ),
+                  ]
+                : [],
+          ),
+        ],
       ),
     );
   }
