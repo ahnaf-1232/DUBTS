e04b65c710e7d951343bfeff4d8a9d212836d26f
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2023-06-28 13:33:08 +0600

    Marker changed
    Markers update according to db change

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -3,7 +3,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:flutter_map/plugin_api.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,80 +13,73 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late MapController _mapController;
-  late Stream<Query> stream;
-  FirebaseDatabase database = FirebaseDatabase.instance;
-  final rtdb = FirebaseDatabase.instanceFor(
-      app: Firebase.app(),
-      databaseURL: 'https://dubts-a851d-default-rtdb.firebaseio.com/');
-  late DatabaseReference locationRef = rtdb.ref().child('location');
+  late DatabaseReference locationRef;
   List<Marker> markers = [];
 
   @override
   void initState() {
     super.initState();
     _mapController = MapController();
-    _startGettingLocations();
+    _initializeFirebase();
   }
 
-  Future<void> _startGettingLocations() async {
-    final data = await locationRef.get();
-    Map location = data.value as Map;
-    if (data.exists) {
-      print(data.value);
+  Future<void> _initializeFirebase() async {
+    await Firebase.initializeApp();
+    final rtdb = FirebaseDatabase.instance;
+    locationRef = rtdb.reference().child('location');
+    locationRef.onValue.listen((event) {
       setState(() {
-        location.forEach((key, value) {
-          print('Key: $key');
-          value.forEach((innerKey, innerValue) {
-            final lng = innerValue['lng'];
-            final lat = innerValue['lat'];
-            var marker = Marker(
-              width: 40.0,
-              height: 40.0,
-              point: LatLng(lat, lng),
-              builder: (ctx) => Container(
-                child: Icon(
-                  Icons.location_pin,
-                  color: Colors.red,
-                  size: 40.0,
-                ),
-              ),
-            );
+        markers = _createMarkersFromData(event.snapshot.value as Map);
+      });
+    });
+  }
 
-            markers.add(marker);
-          });
+  List<Marker> _createMarkersFromData(Map? data) {
+    List<Marker> newMarkers = [];
+    if (data != null) {
+      data.forEach((key, value) {
+        value.forEach((innerKey, innerValue) {
+          final lng = innerValue['lng'];
+          final lat = innerValue['lat'];
+          var marker = Marker(
+            width: 40.0,
+            height: 40.0,
+            point: LatLng(lat, lng),
+            builder: (ctx) => Container(
+              child: Icon(
+                Icons.location_pin,
+                color: Colors.red,
+                size: 40.0,
+              ),
+            ),
+          );
+
+          newMarkers.add(marker);
         });
       });
-    } else {
-      print('No data available.');
     }
+    return newMarkers;
   }
 
   @override
   Widget build(BuildContext context) {
-    DatabaseReference db = FirebaseDatabase.instance.ref().child('location');
     return MaterialApp(
       home: Scaffold(
-        body: StreamBuilder(
-          stream: locationRef.onValue,
-          builder: (context, snapshot) {
-            print('FlutterMap builder called');
-            return FlutterMap(
-              mapController: _mapController,
-              options: MapOptions(
-                center: LatLng(23.6850, 90.3563),
-                zoom: 7,
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'com.example.app',
-                ),
-                MarkerLayer(
-                  markers: markers,
-                ),
-              ],
-            );
-          },
+        body: FlutterMap(
+          mapController: _mapController,
+          options: MapOptions(
+            center: LatLng(23.6850, 90.3563),
+            zoom: 7,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
+            MarkerLayer(
+              markers: markers,
+            ),
+          ],
         ),
       ),
     );
